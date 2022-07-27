9f73936c9ed8a9b5c89004856a76d750efa0e39c
mentat <mentat4231@gmail.com>
2022-07-27 13:49:27 -0400

    solved reader problem, added switch for input, need struct for agg

--- tradeRecords.go
+++ tradeRecords.go
@@ -5,32 +5,55 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type trade struct {
-	id     int64   `json:"id"`
-	market int64   `json:"market"`
-	price  float64 `json:"price"`
-	volume float64 `json:volume`
-	is_buy bool    `json:"is_buy"`
+	Id     json.Number `json:"id"`
+	Market json.Number `json:"market"`
+	Price  json.Number `json:"price"`
+	Volume json.Number `json:"volume"`
+	Is_buy bool        `json:"is_buy"`
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-
-	var t trade
+	//var t trade
 
 	for scanner.Scan() {
-		if scanner.Text() == "BEGIN" {
-			fmt.Println("Market Open:")
-		}
-		fmt.Println(scanner.Text())
-		x := json.Unmarshal([]byte(scanner.Text()), &t)
-		// new func for routune?
-		fmt.Println(t.id, t.market, x)
+		var t trade
 
+		switch scanner.Text() {
+
+		case "BEGIN":
+			fmt.Println("Market Open:")
+		case "END":
+			break
+		default:
+			x := json.NewDecoder(strings.NewReader(scanner.Text())).Decode(&t)
+
+			// new func for routune?
+			if x != nil {
+				fmt.Println(scanner.Text(), "Wont work because", x)
+				break
+
+			} else {
+				fmt.Println(t.Id)
+				break
+			}
+
+		}
+
+		/*
+			if scanner.Text() == "BEGIN" {
+				fmt.Println("Market Open:")
+			}
+			fmt.Println(scanner.Text())
+			x := json.Unmarshal([]byte(scanner.Text()), &t)
+			// new func for routune?
+			fmt.Println(t.id, t.market, x)
+			break
+		*/
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+
 }
