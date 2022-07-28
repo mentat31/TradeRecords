3bc261266f3ca6582499b159701f3717cb29a075
mentat <mentat4231@gmail.com>
2022-07-28 16:54:04 -0400

    almost there, just need to fix percentage_buy

--- tradeRecords.go
+++ tradeRecords.go
@@ -18,15 +18,23 @@ type trade struct {
 }
 
 type record struct {
-	Market         json.Number `type:"integer" required:"true"`
-	TotalVolume    float64
-	MeanPrice      float64
-	MeanVolume     float64
-	VWAP           float64
-	Percentage_buy float64
+	Market         json.Number `json:"market"`
+	TotalVolume    float64     `json:"Total Volume"`
+	MeanPrice      float64     `json:"Mean Price"`
+	MeanVolume     float64     `json:"Mean Volume"`
+	VWAP           float64     `json:"VWAP"`
+	Percentage_buy float64     `json:"Percentage Buy"`
 }
 
-type recTest map[json.Number][]float64
+type persist struct {
+	Market         json.Number
+	Volume         []float64
+	Prices         []float64
+	Percentage_buy float64
+	VWAP           float64
+}
+
+type recTest map[json.Number]persist
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -35,6 +43,7 @@ func main() {
 	//test := make([]record, 5)
 	for scanner.Scan() {
 		var t trade
+		m := persist{}
 
 		switch scanner.Text() {
 
@@ -50,21 +59,43 @@ func main() {
 			//r := &rec
 			// new func for routune?
 			if x != nil {
-				fmt.Printf("%+v %v", t, x)
+				fmt.Println(x)
+				continue
 			}
 
 			i, _ := t.Price.Float64()
 			j, _ := t.Volume.Float64()
 
-			s[t.Market] = append(s[t.Market], i)
-			s[t.Market] = append(s[t.Volume], j)
+			m.Prices = append(m.Prices, i)
+			m.Volume = append(m.Volume, j)
+			sumvol, _ := stats.Sum(m.Volume)
+			m.VWAP = (i * j) / sumvol
+
+			if t.Is_buy == true {
+				m.Percentage_buy += 1.0
+			}
+			s[t.Market] = m
 		}
 	}
+	//fmt.Println(s)
 
 	for key, element := range s {
-		trades := len(element)
-		mean, _ := stats.Mean(element)
-		fmt.Println("Market:", key, " Mean Price:", mean, "Trades: ", trades)
+		fin := record{}
+		trades := len(element.Prices)
+		meanP, _ := stats.Mean(element.Prices)
+		meanV, _ := stats.Mean(element.Volume)
+		totV, _ := stats.Sum(element.Volume)
+
+		fin.Market = key
+		fin.TotalVolume = totV
+		fin.MeanPrice = meanP
+		fin.MeanVolume = meanV
+		fin.VWAP = element.VWAP
+		fin.Percentage_buy = (element.Percentage_buy / float64(trades))
+
+		aggData, _ := json.Marshal(fin)
+		fmt.Println(string(aggData))
 
 	}
+
 }
