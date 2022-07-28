f28d0cf8230fe9ee3c9ad996d288f2cd9971c0f6
mentat <mentat4231@gmail.com>
2022-07-28 11:38:24 -0400

    need type conversion for json fields

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tradeRecords
 
 go 1.18
+
+require github.com/montanaflynn/stats v0.6.6 // indirect
--- tradeRecords.go
+++ tradeRecords.go
@@ -5,21 +5,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type trade struct {
 	Id     json.Number `json:"id"`
 	Market json.Number `json:"market"`
-	Price  json.Number `json:"price"`
+	Price  json.Number `json:"price" type:"float" required:"true"`
 	Volume json.Number `json:"volume"`
 	Is_buy bool        `json:"is_buy"`
 }
 
+type record struct {
+	Market         json.Number `type:"integer" required:"true"`
+	TotalVolume    float64
+	MeanPrice      float64
+	MeanVolume     float64
+	VWAP           float64
+	Percentage_buy float64
+}
+
+type recTest map[json.Number][]json.Number
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	//var t trade
 
+	s := recTest{}
+	//test := make([]record, 5)
 	for scanner.Scan() {
 		var t trade
 
@@ -30,19 +43,23 @@ func main() {
 		case "END":
 			break
 		default:
-			x := json.NewDecoder(strings.NewReader(scanner.Text())).Decode(&t)
 
+			x := json.NewDecoder(strings.NewReader(scanner.Text())).Decode(&t)
+			//r := &rec
 			// new func for routune?
 			if x != nil {
-				fmt.Println(scanner.Text(), "Wont work because", x)
-				break
-
-			} else {
-				fmt.Println(t.Id)
+				fmt.Println(x)
 				break
 			}
+			if j, err := strconv.ParseFloat(t.Price, 64); err == nil {
+				fmt.Println(j)
+			}
+			s[t.Market] = append(s[t.Market], t.Price)
 
 		}
 	}
+	for key, element := range s {
 
+		fmt.Println("Market: ", key, "Average Price", element)
+	}
 }
